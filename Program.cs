f39d8b07477abd04a553e2ce3debb59d8be41fef
pyshkin <pyshkin_pavel93@mail.ru>
2024-02-27 20:48:37 +0300

    addcode

--- Program.cs
+++ Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string[] array = { "213", "ouhwr", "8iejfsa", "h5h", "nuoe1", "hey" };
+string[] newArray = new string[array.Length];
+int index = 0;
+
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i].Length <= 3)
+    {
+        newArray[i] = array[i];
+        index++;
+    }    
+    Console.Write($"{newArray[i]} ");
+}
